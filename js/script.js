fd05916eee7ecce9a6a5bbe0ae1fa4f61c52717c
Angelo <angelo.cina@yahoo.it>
2020-12-01 17:07:32 +0100

    Parte logica senza layout

---
+++ js/script.js
@@ -0,0 +1,15 @@
+var app = new Vue ({ //instanzio vue
+  el: "#app",
+  data: {
+    dischi: [] //creo una proprietà email che è un array vuoto in cui pusherò tutti i dischi, ogni disco è un oggetto contennete informazioni
+  },
+  mounted: function () {
+    axios.get('https://flynn.boolean.careers/exercises/api/array/music')
+    .then(risposta => { //con arrow function
+      risposta.data.response.forEach((element) => {
+        this.dischi.push({poster: element.poster, title: element.title, author: element.author, genre: element.genre, year: element.year})
+      });
+    });
+    console.log(this.dischi);
+  }
+});
